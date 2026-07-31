89ead04241d0709c5627b0d655fada6ef2ada634
aryanxfr <aryangupta130104@gmail.com>
2026-08-01 02:34:05 +0530

    V9 migration added

--- src/main/java/com/aryan/ziplink/dto/request/RefreshTokenRequest.java
+++
@@ -1,9 +0,0 @@
-package com.aryan.ziplink.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record RefreshTokenRequest(
-        @NotBlank
-        String refreshToken
-){
-}
