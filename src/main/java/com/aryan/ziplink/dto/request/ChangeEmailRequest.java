2930cc464c5f2055c18872d678d9df786795a13b
aryanxfr <aryangupta130104@gmail.com>
2026-09-03 23:49:48 +0530

    Email Change Request DTO

---
+++ src/main/java/com/aryan/ziplink/dto/request/ChangeEmailRequest.java
@@ -0,0 +1,14 @@
+package com.aryan.ziplink.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record ChangeEmailRequest(
+        @NotBlank(message = "New email is required")
+        @Email(message = "Invalid email format")
+        String newEmail,
+
+        @NotBlank(message = "Password is required")
+        String password
+) {
+}
