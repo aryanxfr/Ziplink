3518f69b864e297794796becbfe6a973870fe154
aryanxfr <aryangupta130104@gmail.com>
2026-09-03 23:20:32 +0530

    Added id

--- src/main/java/com/aryan/ziplink/dto/response/ClickEventResponse.java
+++ src/main/java/com/aryan/ziplink/dto/response/ClickEventResponse.java
@@ -1,8 +1,10 @@
 package com.aryan.ziplink.dto.response;
 
 import java.time.Instant;
+import java.util.UUID;
 
 public record ClickEventResponse(
+        UUID id,
         Instant clickedAt,
         String ipAddress,
         String userAgent,
