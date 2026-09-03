f13b194da7ff43f65e73a67b22f161e62d818b5a
aryanxfr <aryangupta130104@gmail.com>
2026-09-03 23:47:50 +0530

    added findbyemail and deleted methods

--- src/main/java/com/aryan/ziplink/repository/UserRepository.java
+++ src/main/java/com/aryan/ziplink/repository/UserRepository.java
@@ -3,13 +3,16 @@ package com.aryan.ziplink.repository;
 import com.aryan.ziplink.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
-
+    Optional<User> findByEmailAndDeletedFalse(String email);
+    List<User> findAllByDeletedTrueAndDeletedAtBefore(Instant cutoff);
     boolean existsByEmail(String email);
 
     boolean existsByUsername(String username);
