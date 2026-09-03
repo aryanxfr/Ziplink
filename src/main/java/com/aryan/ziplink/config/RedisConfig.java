3b9ce9a72a40e47d0a2cfbe1de0276a02ac595b0
aryanxfr <aryangupta130104@gmail.com>
2026-09-03 23:27:14 +0530

    Redis Improvements

--- src/main/java/com/aryan/ziplink/config/RedisConfig.java
+++ src/main/java/com/aryan/ziplink/config/RedisConfig.java
@@ -3,12 +3,16 @@ package com.aryan.ziplink.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import tools.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 
 import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
-    private CacheProperties cacheProperties;
+    private final CacheProperties cacheProperties;
     public RedisConfig(CacheProperties cacheProperties) {
         this.cacheProperties = cacheProperties;
     }
@@ -16,8 +20,17 @@ public class RedisConfig {
 
     @Bean
     public RedisCacheConfiguration redisCacheConfiguration(){
+        var cacheTypeValidator = BasicPolymorphicTypeValidator.builder()
+                .allowIfSubType("com.aryan.ziplink.dto.cache.")
+                .build();
+
         return RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofHours(cacheProperties.redirectTtlHours()))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(GenericJacksonJsonRedisSerializer.create(builder ->
+                                builder.enableDefaultTyping(cacheTypeValidator))))
                 .disableCachingNullValues();
     }
 
